99bc750de63670949d190266ec944beadb641883
Shaderock <hdv98plus@gmail.com>
2019-10-28 23:04:37 +0200

    working on front-end

--- app/src/main/java/com/example/urlshortener/MainActivity.java
+++ app/src/main/java/com/example/urlshortener/MainActivity.java
@@ -2,24 +2,37 @@ package com.example.urlshortener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     ApiRequestManager apiRequestManager;
+    Button manageLinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manageLinks = findViewById(R.id.btn_manage_links);
+        manageLinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,
+                        ManageLinksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        apiRequestManager = new ApiRequestManager();
-        apiRequestManager.createShortenedLink(this,
-                "https://www.youtube.com/");
+//        apiRequestManager = new ApiRequestManager();
+//        apiRequestManager.createShortenedLink(this,
+//                "https://www.youtube.com/");
     }
 }
---
+++ app/src/main/java/com/example/urlshortener/ManageLinksActivity.java
@@ -0,0 +1,14 @@
+package com.example.urlshortener;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageLinksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_links);
+    }
+}
